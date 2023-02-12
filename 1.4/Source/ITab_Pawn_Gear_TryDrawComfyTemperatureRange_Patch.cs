b66d4b40d29325f716b980298821fdeff8dc3af9
Taranchuk <nfhfyxer@gmail.com>
2023-02-12 05:55:02 +0200

    fixes

---
+++ 1.4/Source/ITab_Pawn_Gear_TryDrawComfyTemperatureRange_Patch.cs
@@ -0,0 +1,30 @@
+﻿using HarmonyLib;
+using RimWorld;
+using UnityEngine;
+using Verse;
+
+namespace PortraitsOfTheRim
+{
+    [HotSwappable]
+    [HarmonyPatch(typeof(ITab_Pawn_Gear), "TryDrawComfyTemperatureRange")]
+    public static class ITab_Pawn_Gear_TryDrawComfyTemperatureRange_Patch
+    {
+        public static bool Prefix(ITab_Pawn_Gear __instance, ref float curY, float width)
+        {
+            var pawn = __instance.SelPawnForGear;
+            if (!pawn.Dead)
+            {
+                if (pawn.RaceProps.Humanlike)
+                {
+                    Rect rect = new Rect(0f, curY, width - ITab_Pawn_Gear_FillTab_Patch.portraitSize - 20, 44);
+                    float statValue = pawn.GetStatValue(StatDefOf.ComfyTemperatureMin);
+                    float statValue2 = pawn.GetStatValue(StatDefOf.ComfyTemperatureMax);
+                    Widgets.Label(rect, "ComfyTemperatureRange".Translate() + ": " + statValue.ToStringTemperature("F0") + " ~ " + statValue2.ToStringTemperature("F0"));
+                    curY += 44f;
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
--- 1.4/Source/PortraitElementDef.cs
+++ 1.4/Source/PortraitElementDef.cs
@@ -18,17 +18,17 @@ namespace PortraitsOfTheRim
             if (req != null)
             {
                 var boolReport = req.Matches(portrait, this);
-                if (portraitLayer == PR_DefOf.PR_Ear)
-                {
-                    if (boolReport.result is false)
-                    {
-                        Log.Message("Cannot get " + this + " for " + portrait.pawn + " because of " + boolReport.report);
-                    }
-                    else
-                    {
-                        Log.Message("Can get " + this + " for " + portrait.pawn);
-                    }
-                }
+                //if (portraitLayer == PR_DefOf.PR_Ear)
+                //{
+                //    if (boolReport.result is false)
+                //    {
+                //        Log.Message("Cannot get " + this + " for " + portrait.pawn + " because of " + boolReport.report);
+                //    }
+                //    else
+                //    {
+                //        Log.Message("Can get " + this + " for " + portrait.pawn);
+                //    }
+                //}
                 
                 return boolReport.result;
             }
